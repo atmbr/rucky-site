a8c4eb55e442c3f2719a5b22a0441c1b835798d4
Atm <117608077+AtomicTuner@users.noreply.github.com>
2022-12-12 22:45:05 -0300

    Update script-slide-default.js

--- stories-rucky-slide/js/script-slide-default.js
+++ stories-rucky-slide/js/script-slide-default.js
@@ -1,35 +1,36 @@
 var swiper = new Swiper(".slide-container", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  sliderPerGroup: 4,
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
     },
-    520: {
-      slidesPerView: 2,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    768: {
-      slidesPerView: 3,
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
     },
-    1000: {
-      slidesPerView: 4,
-    },
-  autoplay: {
+    autoplay: {
       delay: 5000,
       disableOnInteraction: true
     },
